e1ddafc29166baa4b5218a9318dd6405050de161
AIyer-316 <iyeradi11@gmail.com>
2025-10-02 18:08:27 -0400

    Adjusted Stuff

---
+++ src/main/java/frc/robot/commands/TrackToCageCommand.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import static edu.wpi.first.wpilibj2.command.Commands.defer;
+
+import java.util.Set;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class TrackToCageCommand extends SequentialCommandGroup {
+  /** Creates a new TrackToCageCommand. */
+  public TrackToCageCommand(CommandSwerveDrivetrain drivetrain) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+    
+    );
+  }
+}
